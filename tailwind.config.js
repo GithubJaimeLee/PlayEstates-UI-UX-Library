aeedf20a1862ec8ead4c707c97159d8a41cae8a7
Jaime Lee <18038163520@163.com>
2024-03-18 00:27:11 +0800

    add UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  important: true,
+  darkMode: "class",
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Tailwind 会扫描你项目中的所有 JS/JSX/TS/TSX 文件
+    // 这里可以添加其他你希望 Tailwind 扫描的文件路径
+  ],
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        //以下是UIUX响应式断点规范
+        // xs: { max: "575px" }, // 新增的小屏幕断点(特殊情况备用)
+        // => @media (max-width: 575px) { ... }
+        sm: { max: "767px" },
+        // => @media (max-width: 767px) { ... }
+        md: { min: "768px", max: "1023px" },
+        // => @media (min-width: 767px) { ... }
+        lg: "1024px",
+        // => @media (min-width: 1024px) { ... }
+        xl: "1280px",
+        // => @media (min-width: 1280px) { ... }
+        "2xl": "1440px",
+        // => @media (min-width: 1440px) { ... }
+      },
+    },
   },
   plugins: [],
-}
-
+};
